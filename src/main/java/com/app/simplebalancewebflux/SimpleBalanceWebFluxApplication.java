14b96b0706bc77dc81cf13b32f0104f66d1445a6
joordonezo <jorgeluisordonez1995@gmail.com>
2026-01-13 03:37:32 -0500

    first commit

---
+++ src/main/java/com/app/simplebalancewebflux/SimpleBalanceWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.app.simplebalancewebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBalanceWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBalanceWebFluxApplication.class, args);
+	}
+
+}
