afb13125e0f64c4f4279e0f94bb5f7f4e63cc8bc
joordonezo <jorgeluisordonez1995@gmail.com>
2026-02-08 21:02:51 -0500

    implement oauth2 resource and client

--- src/main/java/com/app/simplebalancewebflux/configuration/SecurityConfig.java
+++ src/main/java/com/app/simplebalancewebflux/configuration/SecurityConfig.java
@@ -1,19 +1,15 @@
 package com.app.simplebalancewebflux.configuration;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 
 @Configuration
 @EnableWebFluxSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
@@ -23,6 +19,7 @@ public class SecurityConfig {
                 .authorizeExchange(exchanges -> exchanges
                         .anyExchange().authenticated()
                 )
+                .oauth2Login(Customizer.withDefaults())
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(Customizer.withDefaults())
                 )
--- src/test/java/com/app/simplebalancewebflux/controllers/HomeControllerTest.java
+++ src/test/java/com/app/simplebalancewebflux/controllers/HomeControllerTest.java
@@ -1,19 +1,15 @@
 package com.app.simplebalancewebflux.controllers;
 
 import com.app.simplebalancewebflux.configuration.SecurityConfig;
-import com.app.simplebalancewebflux.services.TokenService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@WebFluxTest({HomeController.class, AuthController.class})
-@Import({SecurityConfig.class, TokenService.class})
+@WebFluxTest(HomeController.class)
+@Import(SecurityConfig.class)
 class HomeControllerTest {
 
     @Autowired
@@ -27,26 +23,6 @@ class HomeControllerTest {
                 .exchange()
                 .expectStatus()
                 .isUnauthorized();
-    }
-
-    @Test
-    void rootWhenAuthenticatedThenSaysHelloUser() throws Exception {
-        EntityExchangeResult<String> result = this.webTestClient
-                .post()
-                .uri("/token")
-                .headers(headers -> headers.setBasicAuth("jorge","password"))
-                .exchange()
-                .expectStatus()
-                .isOk()
-                .expectBody(String.class)
-                .returnResult();
-
-        String token = result.getResponseBody();
-
-        this.webTestClient
-                .get()
-                .uri("/")
-                .header("Authorization", "Bearer "+ token);
     }
 
     @Test
